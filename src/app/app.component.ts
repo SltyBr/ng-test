01a38e384b2c92d70d845a2e9beb3af8987a1a09
Pavel Popov <saltybrain@yandex.ru>
2022-10-23 18:51:42 +0300

    add request with app initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { getUserProfileAction } from 'src/app/auth/store/actions/getUserProfile.action';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-conzylum-test';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.store.dispatch(getUserProfileAction())
+  }
+
+  constructor(private store: Store) {}
 }
