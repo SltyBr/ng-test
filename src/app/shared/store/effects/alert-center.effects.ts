c7f9e3a924854db69cc45d61e6968ec2cdb4052c
Pavel Popov <saltybrain@yandex.ru>
2022-10-24 11:27:14 +0300

    create laert center effects

---
+++ src/app/shared/store/effects/alert-center.effects.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { ToastrService } from 'ngx-toastr';
+import { tap } from 'rxjs/operators';
+import { loginSuccessAction } from 'src/app/auth/store/actions/login.action';
+import { getProductsSuccessAction } from 'src/app/products/store/actions/getProducts.action';
+
+@Injectable()
+export class AlertEffects {
+  afterLoginSuccessEffect$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(loginSuccessAction),
+        tap(() => this.toastr.success('Login success!', '', {
+          timeOut: 1000
+        }))
+      ),
+    { dispatch: false }
+  );
+
+  getProductsSuccessEffect$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(getProductsSuccessAction),
+        tap(() => this.toastr.success('Products loaded!', '', {
+          timeOut: 1000
+        }))
+      ),
+    { dispatch: false }
+  );
+
+  constructor(private actions$: Actions, private toastr: ToastrService) {}
+}
